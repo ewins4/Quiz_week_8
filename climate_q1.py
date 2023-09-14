fe3f691b85155e636129420382b5d782000b8a29
ewins4 <eddiewinston99@gmail.com>
2023-09-14 13:49:59 +1000

    finished question 1

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,22 @@
+#Can you please write some code in climate_q1.py to connect to the database,
+#fetch the values from the database, and populate some Python lists with the
+#corresponding values? I've already written the code to generate the graphs.
+
 import matplotlib.pyplot as plt
-        
+import sqlite3
 years = []
 co2 = []
 temp = []
+conn = sqlite3.connect('climate.db')
+cursor = conn.cursor()
+cursor.execute('''SELECT Year, Temperature, CO2 from climatedata''')
+
+
+for i in cursor.fetchall():
+    years.append(i[0])
+    co2.append(i[1])
+    temp.append(i[2])
+
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
